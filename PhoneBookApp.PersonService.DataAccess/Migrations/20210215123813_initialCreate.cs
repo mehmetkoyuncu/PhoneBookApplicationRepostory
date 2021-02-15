56bffe88c8120eff4198e0092a512b0a1f4230dc
mehmet koyuncu <m.koyuncu53@hotmail.com>
2021-02-15 15:40:39 +0300

    Migration is done named initialCreate. The database has been created.

---
+++ PhoneBookApp.PersonService.DataAccess/Migrations/20210215123813_initialCreate.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace PhoneBookApp.PersonService.DataAccess.Migrations
+{
+    public partial class initialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedTime = table.Column<DateTime>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Surname = table.Column<string>(nullable: true),
+                    CompanyName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CreatedTime = table.Column<DateTime>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    PhoneNum = table.Column<string>(nullable: true),
+                    Mail = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    PersonId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Contacts_Users_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CompanyName", "CreatedTime", "IsDeleted", "Name", "Surname" },
+                values: new object[,]
+                {
+                    { new Guid("480ab9a9-dafd-4c37-99cf-6c34f74af816"), "Bilge Adam", new DateTime(2021, 2, 15, 15, 38, 9, 667, DateTimeKind.Local).AddTicks(5308), false, "Mehmet", "Koyuncu" },
+                    { new Guid("c05736af-3109-4197-b412-34a027d43cc6"), "Setur", new DateTime(2021, 2, 15, 15, 38, 9, 692, DateTimeKind.Local).AddTicks(9661), true, "Name 1", "Name 2" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_PersonId",
+                table: "Contacts",
+                column: "PersonId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
