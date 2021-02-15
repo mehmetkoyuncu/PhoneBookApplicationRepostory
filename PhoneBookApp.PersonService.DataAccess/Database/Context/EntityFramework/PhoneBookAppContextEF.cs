b1221ce9013c8dc09286fb6fc5bff2d7caab03e5
mehmet koyuncu <m.koyuncu53@hotmail.com>
2021-02-15 15:25:22 +0300

    Database context created.

---
+++ PhoneBookApp.PersonService.DataAccess/Database/Context/EntityFramework/PhoneBookAppContextEF.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneBookApp.PersonService.DataAccess.Database.DummyData;
+using PhoneBookApp.PersonService.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBookApp.PersonService.DataAccess.Database.Context.EntityFramework
+{
+    public class PhoneBookAppContextEF:DbContext
+    {
+        public DbSet<Person> Users { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=PhoneBookAppDBTestDBB;Integrated Security=true; User Id=postgres;Password=7973153.mK;");
+        }
+        Datas dummyData = new Datas();
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().HasData(
+                dummyData.Persons
+                );
+        }
+    }
+}
