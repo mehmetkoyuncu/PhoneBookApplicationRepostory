1f47b233b8c0992ce52163e35d70e8c74c963d30
mehmet koyuncu <m.koyuncu53@hotmail.com>
2021-02-17 09:43:07 +0300

    Database created and fake datas added for Report Service.

---
+++ PhoneBookApp.ReportService.DataAccess/Context/DummyData/Datas.cs
@@ -0,0 +1,52 @@
+﻿using PhoneBookApp.ReportService.DataAccess.Consts;
+using PhoneBookApp.ReportService.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBookApp.ReportService.DataAccess.Context.DummyData
+{
+   public class Datas
+    {
+        public List<ReportItem> reportItems { get; set; }
+
+        public Datas()
+        {
+            reportItems = new List<ReportItem>()
+            {
+                new ReportItem
+                {
+                    Id=Guid.NewGuid(),
+                    Location="Istanbul",
+                    PersonCount=5,
+                    PhoneCount=10,
+                    Status=Status.Done
+                },
+                 new ReportItem
+                {
+                    Id=Guid.NewGuid(),
+                    Location="Rize",
+                    PersonCount=3,
+                    PhoneCount=20,
+                    Status=Status.Ready
+                },
+                  new ReportItem
+                {
+                    Id=Guid.NewGuid(),
+                    Location="Antalya",
+                    PersonCount=25,
+                    PhoneCount=50,
+                    Status=Status.Ready
+                },
+                   new ReportItem
+                {
+                    Id=Guid.NewGuid(),
+                    Location="Eskişehir",
+                    PersonCount=35,
+                    PhoneCount=150,
+                    Status=Status.Done
+                },
+            };
+        }
+    }
+}
---
+++ PhoneBookApp.ReportService.DataAccess/Context/EntityFramework/PhoneBookAppReportContextEF.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneBookApp.ReportService.DataAccess.Context.DummyData;
+using PhoneBookApp.ReportService.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBookApp.ReportService.DataAccess.Context.EntityFramework
+{
+    public class PhoneBookAppReportContextEF:DbContext
+    {
+        public DbSet<ReportItem> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=PhoneBookAppReportDB;Integrated Security=true; User Id=postgres;Password=7973153.mK;");
+        }
+        Datas dummyData = new Datas();
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ReportItem>().HasData(
+                dummyData.reportItems
+                );
+        }
+    }
+}
