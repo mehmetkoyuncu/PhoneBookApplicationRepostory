eafdd28b1de082afcbc014dcf76c47f82ad97a63
mehmet koyuncu <m.koyuncu53@hotmail.com>
2021-02-17 09:28:30 +0300

    Entites created for Report Service.

---
+++ PhoneBookApp.ReportService.DataAccess/Consts/Status.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBookApp.ReportService.DataAccess.Consts
+{
+    public enum Status
+    {
+        Done=1,
+        Ready=2
+    }
+}
---
+++ PhoneBookApp.ReportService.DataAccess/Entities/ReportItem.cs
@@ -0,0 +1,16 @@
+﻿using PhoneBookApp.ReportService.DataAccess.Consts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBookApp.ReportService.DataAccess.Entities
+{
+    public class ReportItem
+    {
+        public Guid Id { get; set; }
+        public int PersonCount { get; set; }
+        public string Location { get; set; }
+        public int PhoneCount { get; set; }
+        public Status Status { get; set; }
+    }
+}
